0c333912af4ab3e94017e605260fa00010df7ead
biswakpl <biswa.kpl@gmail.com>
2016-09-23 00:50:02 +0530

    Update Movie_Class.py

--- Movie_Class.py
+++ Movie_Class.py
@@ -1,15 +1,11 @@
-# This module creates the base Movie class
-
-# Create a Class and methods
-#Put classes in a separate file and the actual operation like creating 
-#objects in separate file
+# This module creates the base Movie class which would be used to create the objects
 
 class Movie():
-    def __init__(self, m_title, m_desc, m_logo, m_trailer):
+    def __init__(self, m_title, m_desc, m_logo, m_trailer): #  Initializing the constructor here with 4 attributes
         self.title = m_title
         self.desc = m_desc
         self.logo_url = m_logo
         self.trailer_url = m_trailer
 
     def show_trailer(self):
-    	webbrowser.open(self.movie_trailer_url)
+    	webbrowser.open(self.movie_trailer_url) #  Defining the show trailer function
